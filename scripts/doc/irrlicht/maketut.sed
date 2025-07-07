001bfbd9ba16b4d6a59f4cc476360d0aa05406d9
Radoslaw Grabowski <radoslaw.p.grabowski@gmail.com>
2025-07-07 13:09:09 +0200

    Irrlicht 1.85 source

---
+++ scripts/doc/irrlicht/maketut.sed
@@ -0,0 +1,4 @@
+# run on single_tut.txt which is created in maketutorial.sh (or .bat)
+
+# replace image links as we don't copy the images into example folders but keep them below media folder
+s/img src="/img src="..\/..\/media\//
---
+++ scripts/doc/irrlicht/maketutorial.sh
@@ -0,0 +1,29 @@
+# Create the tutorial.html files in each example folder
+
+# for every folder below examples
+for i in ../../../examples/[012]*; do
+  echo NOW doing $i
+  
+  # uses the main.cpp files to create some file to use as doxygen input
+  sed -f tutorials.sed $i/main.cpp >single_tut.txt
+#  echo tutorials.sed has run
+  
+  # create the html file 
+  doxygen doxygen_tutorial.cfg
+#  echo doxygen has run
+  
+  # Fix the image links
+  sed -f maketut.sed html/example???.html >tutorial.html
+#  echo maketut.sed has run  
+  
+  #move to example folder 
+  mv tutorial.html $i/tutorial.html
+#  echo copied
+  
+  #cleanup
+  rm -r html
+#  echo cleaned
+done
+
+#cleanup
+rm single_tut.txt
